53ae85043b80166b062af038b53320822d5674fd
cooljonc <91816521+cooljonc@users.noreply.github.com>
2022-10-31 22:52:39 +0100

    commit

--- .scripts/linkdotfiles.sh
+++ .scripts/linkdotfiles.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 (return 2>/dev/null) && return
 DOTFILES_DIR="$(dirname -- "$(dirname -- "$(readlink -f -- "$0")")")"
-stow --no-folding --restow --dir="$DOTFILES_DIR" --target="$HOME" . 2>/dev/null
+stow --no-folding --restow --verbose=2 --dir="$DOTFILES_DIR" --target="$HOME" .
