579d89ea59f9555a27b469deb6408cd23289e38f
cooljonc <91816521+cooljonc@users.noreply.github.com>
2022-11-04 14:38:30 +0100

    commit

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -33,7 +33,20 @@ vim.opt.title = true
 vim.opt.visualbell = false
 vim.opt.wrap = true
 
--- plugins specification & configuration
+-- ensure packer.nvim is installed, otherwise install & bootstrap packer.nvim
+local ensure_packer = function()
+	local install_path = vim.fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+	if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+		vim.fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+		vim.cmd [[packadd packer.nvim]]
+		return true
+	end
+	return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+-- packer.nvim plugins specification & configuration
 return require('packer').startup(function()
 	-- packer can manage itself
 	use 'wbthomason/packer.nvim'
@@ -87,4 +100,9 @@ return require('packer').startup(function()
 		inactive_winbar = {},
 		extensions = {}
 	}
+
+	-- automatically set up configuration after bootstrapping packer.nvim
+	if packer_bootstrap then
+		require('packer').sync()
+	end
 end)
