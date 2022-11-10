9012887eae001cd9fd9d961b2d22d5a9f7cc195a
cooljonc <91816521+cooljonc@users.noreply.github.com>
2022-11-10 15:47:15 +0100

    commit

--- .scripts/cleandotfiles.sh
+++ .scripts/cleandotfiles.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 (return 2>/dev/null) && return
 DOTFILES_DIR="$(dirname -- "$(dirname -- "$(readlink -f -- "$0")")")"
 stow --no-folding --delete --verbose=2 --dir="$DOTFILES_DIR" --target="$HOME" .
