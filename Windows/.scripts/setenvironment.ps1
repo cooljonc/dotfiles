649f68d2044f3b77aecee42d70e79befe8a2bf0b
cooljonc <91816521+cooljonc@users.noreply.github.com>
2024-03-10 14:09:23 +0100

    commit

--- Windows/.scripts/setenvironment.ps1
+++ Windows/.scripts/setenvironment.ps1
@@ -1,4 +1,4 @@
-## Edit 'User' Path:
+# Get 'User' Path
 $UserPath = "$([System.Environment]::GetEnvironmentVariable('Path', 'User'))"
 
 # Add ~\.localscripts to 'User' Path
@@ -11,10 +11,8 @@ if (($UserPath -like "*$HOME\.scripts;*") -eq $false) {
     $UserPath += "$HOME\.scripts;"
 }
 
+# Edit 'User' Path
 [System.Environment]::SetEnvironmentVariable('Path', "$UserPath", 'User')
-##
-
-## Set and/or modify other 'User' environment variables:
 
 # Set common user-preference env-vars
 [System.Environment]::SetEnvironmentVariable('EDITOR', 'nvim', 'User')
@@ -22,13 +20,11 @@ if (($UserPath -like "*$HOME\.scripts;*") -eq $false) {
 # Set util-specific data/config/cache/... location env-vars
 # - (less)
 [System.Environment]::SetEnvironmentVariable('LESSHISTFILE', '-', 'User')
-# - (powershell)
-[System.Environment]::SetEnvironmentVariable('POWERSHELL_UPDATECHECK', 'Off', 'User')
 # - (readline)
 [System.Environment]::SetEnvironmentVariable('INPUTRC', "$HOME/.config/readline/inputrc", 'User')
 
 # Set util-specific application-configuration env-vars
 # - (less)
 [System.Environment]::SetEnvironmentVariable('LESS', '-R --tabs=4', 'User')
-
-##
+# - (powershell)
+[System.Environment]::SetEnvironmentVariable('POWERSHELL_UPDATECHECK', 'Off', 'User')
